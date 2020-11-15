a77638c6a52ded09703e5b5e31d204d17a09aec2
Cato Léan Trütschel <cato@truetschel.de>
2020-11-15 19:31:46 +0100

    Implement Location type as spec'ed by lecture slides

---
+++ src/main/java/org/wahlzeit/model/Location.java
@@ -0,0 +1,49 @@
+package org.wahlzeit.model;
+
+import org.wahlzeit.utils.ExceptionHelper;
+
+public final class Location {
+    private static final Coordinate DEFAULT_COORDINATE = new Coordinate();
+
+    private Coordinate fCoordinate;
+
+    public Location() {
+        init(DEFAULT_COORDINATE);
+    }
+
+    public Location(Coordinate coordinate)
+    {
+        if(coordinate == null)
+            ExceptionHelper.ThrowNullArgumentExceptionMessage("coordinate");
+
+        init(coordinate);
+    }
+
+    private void init(Coordinate coordinate)
+    {
+        fCoordinate = coordinate;
+    }
+
+    public Coordinate getCoordinate()
+    {
+        return fCoordinate;
+    }
+
+    public boolean isDefault()
+    {
+        return fCoordinate == DEFAULT_COORDINATE;
+    }
+
+    public void setCoordinate(Coordinate coordinate)
+    {
+        if(coordinate == null)
+            ExceptionHelper.ThrowNullArgumentExceptionMessage("coordinate");
+
+        init(DEFAULT_COORDINATE);
+    }
+
+    public void resetCoordinate()
+    {
+        init(DEFAULT_COORDINATE);
+    }
+}
